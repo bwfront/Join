b9aca7733e35debc7ca8d7838c8729acc8e92c4c
Bennet Witczak <bennet.witczak@icloud.com>
2023-10-13 15:39:05 +0200

    Include Highlight Navigation Script

--- script_include_html.js
+++ script_include_html.js
@@ -2,11 +2,16 @@ async function init() {
   await includeHTML();
 }
 
-async function includeHTML() {
+/**
+ * Import the Navigation from desktop_template and mobile_template 
+ * when called
+ * @param {String} site 
+ */
+async function includeHTML(site) {
   let includeElements = document.querySelectorAll('[w3-include-html]');
   for (let i = 0; i < includeElements.length; i++) {
     const element = includeElements[i];
-    file = element.getAttribute('w3-include-html'); // "includes/header.html"
+    file = element.getAttribute('w3-include-html');
     let resp = await fetch(file);
     if (resp.ok) {
       element.innerHTML = await resp.text();
@@ -14,6 +19,38 @@ async function includeHTML() {
       element.innerHTML = 'Page not found';
     }
   }
+  navigationHighlight(site);
 }
 
+/**
+ *  Checks wich Site is currently open
+ * @param {String} site 
+ */
+function navigationCheck(site){
+  if(site == 'summary'){
+    navigationHighlight('summary');
+  }else if(site == 'addtask') {
+    navigationHighlight('addtask');
+  }else if(site == 'board'){
+    navigationHighlight('board');
+  }else if(site == 'contacts'){
+    navigationHighlight('contacts');
+  }else if(site == 'privacypolicy'){
+    navigationHighlight('privacypolicy');
+  }else if(site == 'legalnotice'){
+    navigationHighlight('legalnotice');
+  }
+}
 
+/**
+ * Highlight (CSS) Background the currently open Site
+ * Checks if @property {string} site + Mobile exist because 
+ * Mobile Users dont have in Navigation Bar Privacy Policy / Legal Notice
+ * @param {String} site 
+ */
+function navigationHighlight(site){
+  highlightDesktop = document.getElementById(`${site}Desktop`).className += ' desktop-active';
+  if(document.getElementById(`${site}Mobile`)){
+    highlightMobile = document.getElementById(`${site}Mobile`).className += ' mobile-active';
+  }
+}
