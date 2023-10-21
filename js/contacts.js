0262c58fd3fddbd68fc66bb72b58db1e28a0017a
aledeaconu <alexandra.de15@yahoo.com>
2023-10-21 16:27:54 +0200

    contacts html ready

--- js/contacts.js
+++ js/contacts.js
@@ -1,31 +1,112 @@
-let contacts = [
+let allContacts = [
   {
-    name: 'Axel Baum',
-    email: 'axel.baum@gmail.com',
+    name: "Axel Baum",
+    email: "axel.baum@gmail.com",
   },
+  {
+    name: "Axel Baum",
+    email: "axel.baum@gmail.com",
+  },
+  {
+    name: "Axel Baum",
+    email: "axel.baum@gmail.com",
+  },
+  {
+    name: "Axel Baum",
+    email: "axel.baum@gmail.com",
+  },
+  {
+    name: "Axel Baum",
+    email: "axel.baum@gmail.com",
+  },
+  {
+    name: "Axel Baum",
+    email: "axel.baum@gmail.com",
+  },
+  {
+    name: "Axel Baum",
+    email: "axel.baum@gmail.com",
+  },
+  {
+    name: "Axel Baum",
+    email: "axel.baum@gmail.com",
+  },
+
+  {
+    name: "Axel Baum",
+    email: "axel.baum@gmail.com",
+  },
+  {
+    name: "Axel Baum",
+    email: "axel.baum@gmail.com",
+  },
+  {
+    name: "Axel Baum",
+    email: "axel.baum@gmail.com",
+  },
+  {
+    name: "Axel Baum",
+    email: "axel.baum@gmail.com",
+  },
+  {
+    name: "Axel Baum",
+    email: "axel.baum@gmail.com",
+  },
+  {
+    name: "Axel Baum",
+    email: "axel.baum@gmail.com",
+  },{
+    name: "Axel Baum",
+    email: "axel.baum@gmail.com",
+  },
+
 ];
 
+let user = []
+
+
+/**
+ * initialize script for contacts.html
+ */
 function initContacts() {
   renderContacts();
 }
+
+/**
+ * renders the contacts
+ */
 
 function renderContacts() {
   let contactList = document.getElementById('contact_list');
   contactList.innerHTML = '';
 
-  for (let i = 0; i < contacts.length; i++) {
-    let contact = contacts[i];
+  for (let i = 0; i < allContacts.length; i++) {
+    let contact = allContacts[i];
     contactList.innerHTML += generateContacts(contact);
   }
 }
 
+/**
+ * 
+ * generates the contact list
+ * @param {*} contact 
+ * @returns 
+ */
+
 function generateContacts(contact) {
   return `
-  <div class="contact">
-    <h2>${contact.name}</h2>
-    <h3>${contact.email}</h3>
+  <div class="sidebar-contacts">
+    <div>
+      <img src = "../assets/img/profile_badge.png">
+    </div>
+    <div>
+      <h2>${contact.name}</h2>
+      <h3>${contact.email}</h3>
+    </div>
   </div>`;
 }
+
+
 
 function addContact() {
   const newName = 'New Contact';
@@ -35,6 +116,6 @@ function addContact() {
     name: newName,
     email: newEmail,
   };
-  contacts.push(newContact);
+  allContacts.push(newContact);
   renderContacts();
 }
